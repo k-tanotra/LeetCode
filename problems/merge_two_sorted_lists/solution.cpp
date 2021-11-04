07ffa5d2d2124f5bc6e879ef2a6f948e459e16fd
Kanishk Tanotra <59711945+k-tanotra@users.noreply.github.com>
2021-11-04 09:26:46 +0000

    Sync LeetCode submission - Merge Two Sorted Lists (cpp)

---
+++ problems/merge_two_sorted_lists/solution.cpp
@@ -0,0 +1,74 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        
+        ListNode* cur1 = l1;
+        ListNode* cur2 = l2;
+        ListNode* head=nullptr;
+        ListNode* ans = nullptr;
+        while(cur1 && cur2){
+            if(cur1->val<=cur2->val){
+                if(ans==nullptr){
+                    ans = cur1;
+                    head = cur1;
+                    cur1 = cur1->next;
+                }
+                else{
+                    head->next = cur1;
+                    head = head->next;
+                    cur1 = cur1->next;
+                }
+            }
+            else{
+                if(ans==nullptr){
+                    ans = cur2;
+                    head = cur2;
+                    cur2 = cur2->next;
+                }
+                else{
+                    head->next = cur2;
+                    head = head->next;
+                    cur2 =cur2->next;
+                }
+            }
+        }
+        
+        while(cur1){
+            if(ans==nullptr){
+                ans = cur1;
+                head = cur1;
+                cur1 = cur1->next;
+            }
+            else{
+                head->next = cur1;
+                head = head->next;
+                cur1 = cur1->next;
+            }
+        }
+        
+        while(cur2){
+            if(ans==nullptr){
+                ans = cur2;
+                head = cur2;
+                cur2  = cur2->next;
+            }
+            else{
+                head->next = cur2;
+                cur2 = cur2->next;
+                head = head->next;
+            }
+        }
+        
+        return ans;
+    }
+};
