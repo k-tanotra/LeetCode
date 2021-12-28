7d501ae7dee3efd80e0768ac73c5537b40987459
Kanishk Tanotra <59711945+k-tanotra@users.noreply.github.com>
2021-12-28 23:07:04 +0000

    Sync LeetCode submission - Transpose Matrix (cpp)

---
+++ problems/transpose_matrix/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        vector<vector<int>> res;
+        for(int i=0;i<matrix[0].size();i++){
+            vector<int> temp;
+            for(int j=0;j<matrix.size();j++){
+                temp.push_back(matrix[j][i]);
+            }
+            res.push_back(temp);
+        }
+        return res;
+    }
+};
