21431ab1d17b9e85727a8bd394b7934ec5048d42
Kanishk Tanotra <59711945+k-tanotra@users.noreply.github.com>
2022-01-03 09:13:57 +0000

    Sync LeetCode submission - Next Permutation (cpp)

--- problems/next_permutation/solution.cpp
+++ problems/next_permutation/solution.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    int find(vector<int> nums,int s,int val){
-        int diff = INT_MAX;
-        int ans = -1;
-        for(int i=s;i<nums.size();i++){
-            if(nums[i]>val && nums[i]-val<=diff){
-                diff = nums[i]-val;
-                ans = i;
-            }
-        }
-        return ans;
-    }
     void nextPermutation(vector<int>& nums) {
-       int pivot=0;
-        for(int i=nums.size()-1;i>0;i--){
-            if(nums[i-1]<nums[i]){
-                pivot=i;
-                //find closest number
-                int k = find(nums,pivot,nums[i-1]);
-                int temp = nums[k];
-                nums[k] = nums[i-1];
-                nums[i-1] = temp;
+        int firstDec = INT_MAX;
+        for(int i=nums.size()-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                firstDec = min(firstDec,i);
                 break;
             }
         }
-        sort(nums.begin()+pivot,nums.end());
+        if(firstDec==INT_MAX){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        int mn = INT_MAX;
+        int swpIdx = -1;
+        for(int i=nums.size()-1;i>firstDec;i--){
+            if( nums[i]>nums[firstDec]){
+                swpIdx = i;
+                break;
+            }
+        }
+        swap(nums[swpIdx],nums[firstDec]);
+        reverse(nums.begin()+firstDec+1,nums.end());
+        return;
     }
 };
