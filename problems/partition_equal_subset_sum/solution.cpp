5c120b106d4942f68ec0dc641d5bbae6a8df568f
Kanishk Tanotra <59711945+k-tanotra@users.noreply.github.com>
2022-01-06 01:12:51 +0000

    Sync LeetCode submission - Partition Equal Subset Sum (cpp)

---
+++ problems/partition_equal_subset_sum/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum = 0;
+        for(int i=0;i<nums.size();i++){
+            sum+= nums[i];
+        }
+        if(sum%2!=0){
+            return false;
+        }
+        sum = sum/2;
+        vector<bool> dp(sum+1,false);
+        dp[0] = true;
+        
+        for(int i=0;i<nums.size();i++){
+            for(int j=sum;j>=nums[i];j--){
+                dp[j] = dp[j] || dp[j-nums[i]];
+            }
+        }
+        
+        return dp[sum];
+    }
+};
