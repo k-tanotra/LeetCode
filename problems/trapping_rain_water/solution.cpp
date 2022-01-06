f372e0175bd0719d85465157e0620363264ae91c
Kanishk Tanotra <59711945+k-tanotra@users.noreply.github.com>
2022-01-06 11:31:25 +0000

    Sync LeetCode submission - Trapping Rain Water (cpp)

--- problems/trapping_rain_water/solution.cpp
+++ problems/trapping_rain_water/solution.cpp
@@ -1,15 +1,29 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int mx = 0;
         int start = 0;
         int end = height.size()-1;
+        int mxLeft = INT_MIN;
+        int mxRight = INT_MIN;
         int ans = 0;
         while(start<end){
-            int cur_min = min(height[start],height[end]);
-            cur_min<mx ? ans+=mx-cur_min : mx=cur_min;
-            height[start]<=height[end] ? start++ : end--;
+            mxLeft = max(height[start],mxLeft);
+            mxRight = max(height[end],mxRight);
+            if(height[start]<min(mxLeft,mxRight)){
+                ans+= min(mxLeft,mxRight) - height[start];
+            }
+            else if(height[end]<min(mxLeft,mxRight)){
+                ans += min(mxLeft,mxRight) - height[end];
+            }
+            
+            if(height[start]<=height[end]){
+                start++;
+            }
+            else{
+                end--;
+            }
         }
+        
         return ans;
     }
 };
