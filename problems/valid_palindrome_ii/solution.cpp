e8c4c1ecfef71da3afb5d74c922adaca958ab8ec
Kanishk Tanotra <59711945+k-tanotra@users.noreply.github.com>
2022-01-06 01:22:54 +0000

    Sync LeetCode submission - Valid Palindrome II (cpp)

--- problems/valid_palindrome_ii/solution.cpp
+++ problems/valid_palindrome_ii/solution.cpp
@@ -1,45 +1,25 @@
 class Solution {
 public:
-    bool validPalindrome(string s) {
-        int start = 0;
-        int end = s.length()-1;
-        int c =0;
+    bool check(string s,int start,int end){
         while(start<=end){
-            //cout<<start<<" "<<end<<endl;
-            if(s[start]!=s[end]){
-                c++;
-                int stemp = start;
-                int etemp = end;
-                stemp++;
-                while(stemp<=etemp && s[stemp]==s[etemp]){
-                    stemp++;
-                    etemp--;
-                }
-                if(stemp>etemp){
-                    return true;
-                }
-                //cout<<start<<" "<<end<<endl;
-                
-                stemp = start;
-                etemp = end;
-                etemp--;
-                //cout<<start<<" "<<end<<endl;
-                while(stemp<=etemp && s[stemp]==s[etemp]){
-                    
-                    stemp++;
-                    etemp--;
-                }
-                if(stemp>etemp){
-                    return true;
-                }
-                return false;
-            }
-            else{
-                start++;
-                end--;
-            }
-        }
-        
+           if(s[start]!=s[end]){
+               return false;
+           }
+           start++;
+           end--;
+       }
+       return true;
+    }
+    bool validPalindrome(string s) {
+       int start = 0;
+       int end = s.length()-1;
+       while(start<=end){
+           if(s[start]!=s[end]){
+               return check(s,start+1,end) || check(s,start,end-1);
+           }
+           start++;
+           end--;
+       }
         return true;
     }
 };
