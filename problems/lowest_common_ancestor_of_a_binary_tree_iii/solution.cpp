0eadd9719250d9631a90128f1e5423de929ec67c
Kanishk Tanotra <59711945+k-tanotra@users.noreply.github.com>
2022-01-10 22:54:10 +0000

    Sync LeetCode submission - Lowest Common Ancestor of a Binary Tree III (cpp)

--- problems/lowest_common_ancestor_of_a_binary_tree_iii/solution.cpp
+++ problems/lowest_common_ancestor_of_a_binary_tree_iii/solution.cpp
@@ -11,29 +11,16 @@ public:
 
 class Solution {
 public:
-    unordered_set<int> s;
-    void travel(Node* p,Node* &ans,bool &found){
-        if(p==nullptr){
-            return;
-        }
-        if(found){
-            return;
-        }
-        if(s.find(p->val)==s.end()){
-            s.insert(p->val);
-        }
-        else{
-            ans = p;
-            found = true;
-            return;
-        }
-        travel(p->parent,ans,found);
-    }
     Node* lowestCommonAncestor(Node* p, Node * q) {
-        Node* ans;
-        bool found = false;
-        travel(p,ans,found);
-        travel(q,ans,found);
-        return ans;
+       // int count = 0;
+        Node* a = p;
+        Node* b = q;
+        while(p!=q){
+            //count++;
+            p = p==nullptr? b:p->parent;
+            q = q==nullptr? a:q->parent;
+        }
+      //  cout<<count;
+        return p;
     }
 };
