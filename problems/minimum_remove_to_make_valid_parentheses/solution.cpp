b0c1f5bfa15f06d12bce4b1be8993a98d8b78ae0
Kanishk Tanotra <59711945+k-tanotra@users.noreply.github.com>
2022-01-10 00:00:49 +0000

    Sync LeetCode submission - Minimum Remove to Make Valid Parentheses (cpp)

--- problems/minimum_remove_to_make_valid_parentheses/solution.cpp
+++ problems/minimum_remove_to_make_valid_parentheses/solution.cpp
@@ -1,46 +1,40 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-        unordered_set<int> rem;
-        stack<pair<char,int>> st;
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='('){
-                //cout<<"hi"<<endl;
-                while(!st.empty() && st.top().first==')'){
-                    rem.insert(st.top().second);
-                    st.pop();
-                }
-                st.push({'(',i});
-            }
-            else if(s[i]==')'){
-                //cout<<"bye"<<endl;
-                if(!st.empty() && st.top().first=='('){
-                    //cout<<"well"<<endl;
-                    st.pop();
-                }
-                else{
-                    rem.insert(i);
-                }
-            }
-        }
-        //cout<<"well"<<endl;
-        /*
-        for(auto itr=rem.begin();itr!=rem.end();itr++){
-            cout<<s[*itr]<<" "<<*itr<<endl;
-        }
-        cout<<endl;
-        */
-        while(!st.empty()){
-            rem.insert(st.top().second);
-            st.pop();
-        }
-        
+        int count = 0;
         string ans = "";
         for(int i=0;i<s.length();i++){
-            if(rem.find(i)==rem.end()){
-                ans += s[i];
+            if(s[i]=='('){
+                count++;
+            }
+            else if(s[i]==')'){
+                count--;
+                if(count<0){
+                    s[i] = '*';
+                    count=0;
+                }
             }
         }
+        
+        count =0;
+        for(int i=s.length()-1;i>=0;i--){
+            if(s[i]==')'){
+                count++;
+            }
+            else if(s[i]=='('){
+                count--;
+                if(count<0){
+                    s[i]='*';
+                    count=0;
+                }
+            }
+        }
+        
+        for(int i=0;i<s.length();i++){
+            if(s[i]!='*')
+                ans+=s[i];
+        }
+        
         return ans;
     }
 };
