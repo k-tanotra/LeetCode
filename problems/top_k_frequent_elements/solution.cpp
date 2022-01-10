fcd35876eb1f7804e836bed7e95eb8fd3a617192
Kanishk Tanotra <59711945+k-tanotra@users.noreply.github.com>
2022-01-10 09:34:33 +0000

    Sync LeetCode submission - Top K Frequent Elements (cpp)

--- problems/top_k_frequent_elements/solution.cpp
+++ problems/top_k_frequent_elements/solution.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        map<int,int> m;
-        vector<int> ans;
+        //bucket  sort O(n)
+        unordered_map<int,int> m;
         for(int i=0;i<nums.size();i++){
-            if(m.find(nums[i])==m.end()){
-                m.insert({nums[i],1});
-            }
-            else{
-                m[nums[i]]++;
-            }
+            m[nums[i]]++;
         }
-        priority_queue<pair<int,int>> pq;
+        vector<vector<int>> buckets(nums.size()+1);
         for(auto itr=m.begin();itr!=m.end();itr++){
-            pq.push({itr->second,itr->first});
+            buckets[itr->second].push_back(itr->first);
         }
         
-        for(int i=0;i<k;i++){
-            ans.push_back(pq.top().second);
-            pq.pop();
+        vector<int> ans;
+        int sz = 0;
+        for(int i=buckets.size()-1;i>=0 && sz!=k;i--){
+            for(int j=0;j<buckets[i].size() && sz!=k;j++){
+                ans.push_back(buckets[i][j]);
+                sz++;
+            }
         }
-        
         return ans;
     }
 };
