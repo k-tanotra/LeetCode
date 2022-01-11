1fc7ff3b48df89c30b63062e73ca256cb174adcf
Kanishk Tanotra <59711945+k-tanotra@users.noreply.github.com>
2022-01-11 22:49:54 +0000

    Sync LeetCode submission - Custom Sort String (cpp)

--- problems/custom_sort_string/solution.cpp
+++ problems/custom_sort_string/solution.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     string customSortString(string order, string s) {
-       unordered_map<char,int> m;
-       unordered_map<int,char> m2;
-       int p = 26;
-       for(int i=0;i<order.length();i++){
-           m[order[i]] = p;
-           m2[p] = order[i];
-           p--;
-       }
-       priority_queue<int> pq;
-       for(int i=0;i<s.length();i++){
-           if(m.find(s[i])==m.end()){
-               m[s[i]] = p;
-               m2[p] = s[i];
-               p--;
-           }
-           pq.push(m[s[i]]);
-       }
+        unordered_map<char,int> m;
+        for(int i=0;i<s.length();i++){
+            m[s[i]]++;
+        }
         
-       string ans="";
-        while(!pq.empty()){
-            ans += m2[pq.top()];
-            pq.pop();
+        string ans = "";
+        for(int i=0;i<order.length();i++){
+            if(m.find(order[i])!=m.end()){
+                while(m[order[i]]>0){
+                    ans+=order[i];
+                    m[order[i]]--;
+                }
+            }
+        }
+        
+        for(auto itr=m.begin();itr!=m.end();itr++){
+            while(itr->second>0){
+                ans+=itr->first;
+                itr->second--;
+            }
         }
         
         return ans;
