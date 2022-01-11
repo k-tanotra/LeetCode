2c90468d199bff45c9475a33fcb1421022aeb012
Kanishk Tanotra <59711945+k-tanotra@users.noreply.github.com>
2022-01-11 06:27:48 +0000

    Sync LeetCode submission - Vertical Order Traversal of a Binary Tree (cpp)

--- problems/vertical_order_traversal_of_a_binary_tree/solution.cpp
+++ problems/vertical_order_traversal_of_a_binary_tree/solution.cpp
@@ -11,45 +11,47 @@
  */
 class Solution {
 public:
-    static bool cmp(pair<int,int> a, pair<int,int> b){
-        if(a.second<b.second){
-            return true;
-        }
-        else if(a.second==b.second){
-            if(a.first<b.first){
-                return true;
-            }
-        }
-        return false;
-    }
-    map<int,vector<pair<int,int>>> m;
-    void dfs(TreeNode* root,int pos,int depth){
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
         if(root==nullptr){
-            return;
+            return {};
         }
-        if(m.find(pos)==m.end()){
-            vector<pair<int,int>> x;
-            x.push_back({root->val,depth});
-            m.insert({pos,x});
-        }
-        else{
-            m[pos].push_back({root->val,depth});
+        queue<pair<int,TreeNode*>> q;
+        unordered_map<int,unordered_map<int,vector<int>>> m;
+        q.push({0,root});
+        int depth = 0;
+        int mnCol = 0;
+        int mxCol = 0;
+        while(!q.empty()){
+            int n = q.size();
+            while(n>0){
+                if(q.front().second->left){
+                    q.push({q.front().first-1,q.front().second->left});
+                    mnCol = min(mnCol,q.front().first-1);
+                }
+                if(q.front().second->right){
+                    q.push({q.front().first+1,q.front().second->right});
+                    mxCol = max(mxCol,q.front().first+1);
+                }
+                m[q.front().first][depth].push_back(q.front().second->val);
+                q.pop();
+                n--;
+            }
+            depth++;
         }
         
-        dfs(root->left,pos-1,depth+1);
-        dfs(root->right,pos+1,depth+1);
-    }
-    vector<vector<int>> verticalTraversal(TreeNode* root) {
         vector<vector<int>> ans;
-        dfs(root,0,0);
-        for(auto itr=m.begin();itr!=m.end();itr++){
-            sort(itr->second.begin(),itr->second.end(),cmp);
-            vector<int> x;
-            for(int i=0;i<itr->second.size();i++){
-                x.push_back(itr->second[i].first);
+        for(int i=mnCol;i<=mxCol;i++){
+            unordered_map<int,vector<int>> m2 = m[i];
+            vector<int> t;
+            for(int j=0;j<depth;j++){
+                if(m2.find(j)!=m2.end()){
+                    sort(m2[j].begin(),m2[j].end());
+                    t.insert(t.end(),m2[j].begin(),m2[j].end());
+                }
             }
-            ans.push_back(x);
+            ans.push_back(t);
         }
+        
         return ans;
     }
 };
