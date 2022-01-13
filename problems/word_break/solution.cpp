8f5eec454dd0c8f87b1c848bc8147f2caffb6d3b
Kanishk Tanotra <59711945+k-tanotra@users.noreply.github.com>
2022-01-13 23:59:47 +0000

    Sync LeetCode submission - Word Break (cpp)

--- problems/word_break/solution.cpp
+++ problems/word_break/solution.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     unordered_set<string> st;
-    vector<int> dp;
-    bool rec(string s,int idx,int n){
+    int n;
+    bool rec(string s,int idx,vector<int> &dp){
         if(idx>=n){
             return true;
         }
@@ -10,23 +10,22 @@ public:
             string temp = "";
             for(int i=idx;i<n;i++){
                 temp +=s[i];
-                //cout<<temp<<endl;
-                if(st.find(temp)!=st.end() && rec(s,i+1,n)){
-                   dp[idx] = true;
-                   break;
+                if(st.find(temp)!=st.end() && rec(s,i+1,dp)){
+                    dp[idx]=true;
+                    break;
                 }
             }
-            if(dp[idx]!=1)
-                dp[idx] = false;
+            if(dp[idx]==-1)
+                dp[idx] = 0;
         }
         return dp[idx];
     }
     bool wordBreak(string s, vector<string>& wordDict) {
-        for(int i=0;i<wordDict.size();i++){
-            st.insert({wordDict[i]});
+        for(string s:wordDict){
+            st.insert(s);
         }
-        int n = s.length();
-        dp.resize(n,-1);
-        return rec(s,0,n);
+        n = s.length();
+        vector<int> dp(n,-1);
+        return rec(s,0,dp);
     }
 };
