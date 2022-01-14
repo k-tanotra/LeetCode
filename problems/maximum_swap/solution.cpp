63af93ed11e4c1dfd3d14784c82403cc4f0848e8
Kanishk Tanotra <59711945+k-tanotra@users.noreply.github.com>
2022-01-14 00:11:00 +0000

    Sync LeetCode submission - Maximum Swap (cpp)

--- problems/maximum_swap/solution.cpp
+++ problems/maximum_swap/solution.cpp
@@ -3,18 +3,7 @@ public:
     int maximumSwap(int num) {
         string s = to_string(num);
         int n = s.length();
-        //vector<int> fromLeft(n);
         vector<int> fromRight(n);
-        /*
-        int mn = INT_MAX;
-        int idx = -1;
-        for(int i=0;i<n;i++){
-            if(s[i]-'0'<mn){
-                mn = s[i]-'0';
-                idx = i;
-            }
-            fromLeft[i] = idx;
-        }*/
         int mx = INT_MIN;
         int idx= -1;
         for(int i=n-1;i>=0;i--){
@@ -24,16 +13,9 @@ public:
             }
             fromRight[i] = idx;
         }
-        
-        int mn = INT_MAX;
-        idx = -1;
         for(int i=0;i<n;i++){
-            if(s[i]-'0'<mn){
-                mn = s[i]-'0';
-                idx = i;
-            }
-            if(s[idx]<s[fromRight[i]]){
-                swap(s[idx],s[fromRight[i]]);
+            if(s[i]<s[fromRight[i]]){
+                swap(s[i],s[fromRight[i]]);
                 break;
             }
         }
