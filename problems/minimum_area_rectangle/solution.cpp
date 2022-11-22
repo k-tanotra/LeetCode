8b530782792b878c3b7ea88abb9775811b4447a2
Kanishk Tanotra <59711945+k-tanotra@users.noreply.github.com>
2022-11-22 07:00:54 +0000

    Sync LeetCode submission - Minimum Area Rectangle (cpp)

--- problems/minimum_area_rectangle/solution.cpp
+++ problems/minimum_area_rectangle/solution.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
-    int genHash(int x,int y){
-        return 4001*x + y;
-    }
     int minAreaRect(vector<vector<int>>& points) {
-        unordered_set<int> m;
-        for(int i=0;i<points.size();i++){
-            m.insert(genHash(points[i][0],points[i][1]));
-        }
+        int ans = INT_MAX;
+        unordered_set<int> s;
         
-        int area = INT_MAX;
         for(int i=0;i<points.size();i++){
-            int x1 = points[i][0];
-            int y1 = points[i][1];
+            s.insert(points[i][0] + points[i][1]*40001);
+        }
+        for(int i=0;i<points.size();i++){
             for(int j=i+1;j<points.size();j++){
-                int x2 = points[j][0];
-                int y2 = points[j][1];
-                if(x1!=x2 && y1!=y2 && m.find(genHash(x1,y2))!=m.end()  && m.find(genHash(x2,y1))!=m.end()){
-                    area = min(area,abs(x1-x2)*abs(y1-y2));
+                if(points[i][0]!=points[j][0] && points[i][1]!=points[j][1] && s.find(points[i][0] + points[j][1]*40001)!=s.end() && s.find(points[j][0] + points[i][1]*40001)!=s.end()){
+                    ans = min(ans,abs(points[j][0]-points[i][0])*abs(points[j][1]-points[i][1]));
                 }
             }
         }
         
-        return area==INT_MAX ? 0:area;
+        return ans==INT_MAX ? 0 : ans;
     }
 };
