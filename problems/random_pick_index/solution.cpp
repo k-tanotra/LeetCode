b468ba0227aca3558f8d7737ed1a6d85c7b24711
Kanishk Tanotra <59711945+k-tanotra@users.noreply.github.com>
2022-11-30 23:04:09 +0000

    Sync LeetCode submission - Random Pick Index (cpp)

---
+++ problems/random_pick_index/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    unordered_map<int,vector<int>> m;
+    Solution(vector<int>& nums) {
+       for(int i=0;i<nums.size();i++){
+           m[nums[i]].push_back(i);
+       }
+    }
+    
+    int pick(int target) {
+        int idx = rand() %  m[target].size();
+        return m[target][idx];
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(nums);
+ * int param_1 = obj->pick(target);
+ */
