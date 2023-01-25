840d336c4c64fbac93d713c04b4e2bff67e69a04
Kanishk Tanotra <59711945+k-tanotra@users.noreply.github.com>
2023-01-25 21:50:39 +0000

    Sync LeetCode submission - Generate Parentheses (cpp)

--- problems/generate_parentheses/solution.cpp
+++ problems/generate_parentheses/solution.cpp
@@ -1,31 +1,39 @@
 class Solution {
 public:
+    bool check(string s){
+        int c = 0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                c++;
+            }
+            else if(s[i]==')'){
+                c--;
+            }
+            if(c<0){
+                return false;
+            }
+        }
+        return (c==0);
+    }
+    void generate(string cur,vector<string> &res,int left,int right){
+        if(left==0 && right==0){
+            if(check(cur)){
+                res.push_back(cur);
+                return;
+            }
+        }
+
+        if(left>0){
+            generate(cur+'(',res,left-1,right);
+        }
+        if(right>0){
+            generate(cur+')',res,left,right-1);
+        }
+    }
     vector<string> generateParenthesis(int n) {
-       vector<vector<string>> dp(n+1,vector<string>());
-       dp[1] = vector<string>(1,"()");
-       unordered_set<string> st;
-       st.insert("()");
-      // string s;
-       for(int i=2;i<=n;i++){
-           for(int j=1;j<i;j++){
-               //vector<string> x = dp[j];
-               //vector<string> y = dp[i-j];
-               for(int p=0;p<dp[j].size();p++){
-                   for(int q=0;q<dp[i-j].size();q++){
-                       //s = dp[j][p] + dp[i-j][q];
-                       if(st.find(dp[j][p] + dp[i-j][q])==st.end()){
-                           st.insert(dp[j][p] + dp[i-j][q]);
-                           dp[i].push_back(dp[j][p] + dp[i-j][q]);
-                       }
-                   }
-                   if(j==i-1){
-                       //s = "(" + dp[j][p] + ")";
-                       dp[i].push_back("(" + dp[j][p] + ")");
-                   }
-               }
-           }
-       }
-        
-       return dp[n];
+        string cur = "";
+        vector<string> res;
+        generate(cur,res,n,n);
+        return res;
     }
 };
