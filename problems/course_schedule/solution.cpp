b852f5e623b259e326815d7ad1da4888a2f7a5ff
Kanishk Tanotra <59711945+k-tanotra@users.noreply.github.com>
2023-03-19 02:37:04 +0000

    Sync LeetCode submission - Course Schedule (cpp)

--- problems/course_schedule/solution.cpp
+++ problems/course_schedule/solution.cpp
@@ -1,48 +1,47 @@
 class Solution {
 public:
-    unordered_map<int,vector<int>> m;
-    bool dfs(vector<bool> &visited,int cur,set<pair<int,int>> &t){
-        if(visited[cur]){
-            return false;
+    void print(vector<bool> &vis){
+        for(int i=0;i<vis.size();i++){
+            cout<<vis[i]<<" ";
         }
-        vector<int> s = m[cur];
-        for(int i=0;i<s.size();i++){
-            visited[cur]=true;
-            if(t.find({cur,s[i]})==t.end()){
-                if(!dfs(visited,s[i],t)){
-                    return false;
-                }
-            }
-            visited[cur]=false;
-            t.insert({cur,s[i]});
-        }
-        return true;
+        cout<<"\n";
+    }
+    bool dfs(int idx,unordered_map<int,vector<int>> &m,vector<int> &vis){
+       //cout<<idx<<endl;
+      // print(vis);
+       if(vis[idx]==1){
+           return false;
+       }
+       if(vis[idx]==2){
+           return true;
+       }
+       vis[idx] = 1;
+       bool ways = true;
+       for(int i=0;i<m[idx].size();i++){
+           ways = ways && dfs(m[idx][i],m,vis);
+           if(ways==false){
+               vis[idx] = 0;
+               return ways;
+           }
+       }
+       vis[idx] = 2;
+       return ways;
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        m.clear();
-        int n = prerequisites.size();
-        for(int i=0;i<n;i++){
-            if(m.find(prerequisites[i][0])==m.end()){
-                vector<int> x;
-                x.push_back(prerequisites[i][1]);
-                m.insert({prerequisites[i][0],x});
-            }
-            else{
-                m[prerequisites[i][0]].push_back(prerequisites[i][1]);
-            }
+        vector<int> vis(numCourses,false);
+        unordered_map<int,vector<int>> m;
+        for(int i=0;i<prerequisites.size();i++){
+            m[prerequisites[i][0]].push_back(prerequisites[i][1]);
         }
-        vector<bool> visited(numCourses,false);
-        set<pair<int,int>> s;
-        for(int i=0;i<n;i++){
-            visited[prerequisites[i][0]]=true;
-            if(s.find({prerequisites[i][0],prerequisites[i][1]})==s.end()){
-                if(!dfs(visited,prerequisites[i][1],s)){
+        
+        for(int i=0;i<vis.size();i++){
+            if(!vis[i]){
+                if(!dfs(i,m,vis)){
                     return false;
                 }
             }
-            s.insert({prerequisites[i][0],prerequisites[i][1]});
-            visited[prerequisites[i][0]]=false;
         }
+
         return true;
     }
 };
