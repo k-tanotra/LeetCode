3aa6ea24476fd539f1fd0f86f8339d75217daae6
Kanishk Tanotra <59711945+k-tanotra@users.noreply.github.com>
2023-07-25 00:48:59 +0000

    Sync LeetCode submission - Rotate Array (cpp)

--- problems/rotate_array/solution.cpp
+++ problems/rotate_array/solution.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
-    void rev(vector<int> &nums,int start,int end){
-        while(start<=end){
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-            start++;
-            end--;
-        } 
+    void flipAtIndex(vector<int> &nums,int from,int to){
+        while(from<to){
+            swap(nums[from],nums[to]);
+            from++;
+            to--;
+        }
     }
     void rotate(vector<int>& nums, int k) {
-        if(k>nums.size()){
-            k = k%nums.size();
-        }
-        if(k==0){
-            return;
-        }
-        rev(nums,0,nums.size()-1);
-        rev(nums,0,k-1);
-        rev(nums,k,nums.size()-1);
+        k = k%nums.size();
+        flipAtIndex(nums,0,nums.size()-1);
+        flipAtIndex(nums,0,k-1);
+        flipAtIndex(nums,k,nums.size()-1);
     }
 };
