b2727c99d4301484c1b92bd44f043e3be5b4d8b0
Kanishk Tanotra <59711945+k-tanotra@users.noreply.github.com>
2023-07-31 02:45:08 +0000

    Sync LeetCode submission - Ransom Note (cpp)

---
+++ problems/ransom_note/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int> freq1(26,0);
+        vector<int> freq2(26,0);
+        for(int i=0;i<ransomNote.size();i++){
+            freq1[ransomNote[i]-'a']++;
+        }
+        for(int i=0;i<magazine.size();i++){
+            freq2[magazine[i]-'a']++;
+        }
+
+        for(int i=0;i<freq1.size();i++){
+            if(freq1[i]>freq2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
