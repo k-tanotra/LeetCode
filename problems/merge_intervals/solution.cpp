133d9c7982fccf4d2a9353932f81b5225569239d
Kanishk Tanotra <59711945+k-tanotra@users.noreply.github.com>
2023-07-31 05:24:52 +0000

    Sync LeetCode submission - Merge Intervals (cpp)

--- problems/merge_intervals/solution.cpp
+++ problems/merge_intervals/solution.cpp
@@ -4,14 +4,12 @@ public:
         sort(intervals.begin(),intervals.end());
         vector<vector<int>> ans;
         ans.push_back(intervals[0]);
-        int lastIdx = 0;
         for(int i=1;i<intervals.size();i++){
-            if(intervals[i][0]<= ans[lastIdx][1]){
-                ans[lastIdx][1] = max(intervals[i][1],ans[lastIdx][1]);
+            if(ans.back()[1]>=intervals[i][0]){
+                ans.back()[1] = max(intervals[i][1],ans.back()[1]);
             }
             else{
                 ans.push_back(intervals[i]);
-                lastIdx++;
             }
         }
         return ans;
