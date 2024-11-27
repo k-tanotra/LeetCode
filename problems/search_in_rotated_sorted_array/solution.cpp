12814bb24bce3ccb071f46df28ee35aa3eaf3dbc
Kanishk Tanotra <59711945+k-tanotra@users.noreply.github.com>
2024-11-27 02:46:34 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array (cpp)

--- problems/search_in_rotated_sorted_array/solution.cpp
+++ problems/search_in_rotated_sorted_array/solution.cpp
@@ -2,52 +2,29 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int start = 0;
-        int end = nums.size()-1;
-        int rp = -1;
-        int ans = -1;
-        while(start<end){
-            int mid = start + (end-start)/2;
-            if(mid-1>=0 && mid+1<=nums.size() && nums[mid-1]>nums[mid] && nums[mid+1]>nums[mid]){
-                rp = mid;
-                break;
-            }
-            else if(nums[mid]>nums[end]){
-                start = mid+1;
-            }
-            else{
-                end = mid-1;
-            }
-        }
-        if(rp==-1){
-            rp = start;
-        }
-        // cout<<rp<<endl;
-        int st = rp;
-        int next = rp==0? rp =0:rp=rp-1;
-        if(target>=nums[0] && target<=nums[next]){
-            start = 0;
-            end = next;
-        }
-        else{
-            end = nums.size()-1;
-            start = st;
-        }
-        cout<<start<<" "<<end<<endl;
+        int end= nums.size()-1;
         while(start<=end){
-            int mid = start + (end-start)/2;
+            int mid = (start+end)/2;
             if(nums[mid]==target){
-                ans = mid;
-                break;
+                return mid;
             }
-            else if(nums[mid]>target){
-                end = mid-1;
+            if(nums[start]<=nums[mid]){
+                if(nums[start]<= target && target<nums[mid]){
+                    end = mid-1;
+                }
+                else{
+                    start = mid +1;
+                }
             }
             else{
-                start = mid + 1;
+                if(nums[mid]< target && target<=nums[end]){
+                    start = mid+1;
+                }
+                else{
+                    end= mid-1;
+                }
             }
         }
-        
-        return ans;
-        
+        return -1;
     }
 };
