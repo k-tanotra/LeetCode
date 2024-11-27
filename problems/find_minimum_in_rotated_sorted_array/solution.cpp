9e475d7bb5a9205f8887642faa638465cec7f69e
Kanishk Tanotra <59711945+k-tanotra@users.noreply.github.com>
2024-11-27 18:54:55 +0000

    Sync LeetCode submission - Find Minimum in Rotated Sorted Array (cpp)

--- problems/find_minimum_in_rotated_sorted_array/solution.cpp
+++ problems/find_minimum_in_rotated_sorted_array/solution.cpp
@@ -3,17 +3,17 @@ public:
     int findMin(vector<int>& nums) {
         int start = 0;
         int end = nums.size()-1;
-        while(start<end){
-            if(nums[start]<nums[end]) 
-                return nums[start];
-            int mid = (start + end)/2;
-            if(nums[mid]>=nums[end]){
-                start=mid+1;
+        int candidate = INT_MAX;
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(nums[mid]<=nums[end]){
+                candidate = min(nums[mid],candidate);
+                end = end - 1;
             }
-            else if(nums[mid]<nums[end]){
-                end = mid;
+            else{
+                start = start + 1;
             }
         }
-        return nums[start];
+        return candidate;
     }
 };
