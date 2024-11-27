ff5b401a619ccc2629a2677f2bc09de2d31dd062
Kanishk Tanotra <59711945+k-tanotra@users.noreply.github.com>
2024-11-27 19:19:46 +0000

    Sync LeetCode submission - Find Peak Element (cpp)

--- problems/find_peak_element/solution.cpp
+++ problems/find_peak_element/solution.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
-    int findPeakElement(vector<int>& nums) {
-        if(nums.size()==1){
-            return 0;
+    bool isPeak(int a,int b, int c){
+        if(a>b && a>c){
+            return true;
         }
-        int sz = nums.size()-1;
+        return false;
+    }
+    int findPeakElement(vector<int>& nums) {
         int start = 0;
         int end = nums.size()-1;
         while(start<=end){
-            int mid = (end - start)/2 + start;
-            if(mid!=sz && nums[mid]<=nums[mid+1]){
-                start = mid+1;
-            }
-            else if(mid!=0 && nums[mid]<=nums[mid-1]){
-                end = mid-1;
-            }
-            else if( (mid==0 && nums[mid+1]<nums[mid]) || ((mid==sz && nums[mid-1]<nums[mid]))
-                    || (nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]) ){
+            int mid = (start+end)/2;
+            //cout<<start<<" "<<end<<" "<<mid<<endl;
+            int left = (mid-1>= 0) ?  nums[mid-1] : INT_MIN;
+            int right = (mid+1< nums.size()) ?  nums[mid+1] : INT_MIN;
+            //cout<<nums[mid]<<" "<<left<<" "<<right<<endl;
+            if(isPeak(nums[mid],left,right)){
                 return mid;
             }
+            else if(nums[mid]<left){
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
         }
-        return start;
+        return 0;
     }
 };
