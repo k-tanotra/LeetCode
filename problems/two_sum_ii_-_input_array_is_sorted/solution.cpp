ef361c845c5d37589e5c5e7787c339e71cad51d4
Kanishk Tanotra <59711945+k-tanotra@users.noreply.github.com>
2024-11-27 19:33:47 +0000

    Sync LeetCode submission - Two Sum II - Input Array Is Sorted (cpp)

--- problems/two_sum_ii_-_input_array_is_sorted/solution.cpp
+++ problems/two_sum_ii_-_input_array_is_sorted/solution.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        int start = 0;
+        int left = 0;
         int end = numbers.size()-1;
-        while(start<end){
-            int sum = numbers[start] + numbers[end];
-            if(sum>target){
+        while(left<end){
+            if(numbers[left]+numbers[end] == target){
+                return {left+1,end+1};
+            }
+            else if(numbers[left]+numbers[end] > target){
                 end--;
             }
-            else if(sum<target){
-                start++;
-            }
             else{
-                return {start+1,end+1};
+                left++;
             }
         }
         return {};
