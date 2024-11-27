03d805aca3fcc5b98354e69f1d2239886a86c3c6
Kanishk Tanotra <59711945+k-tanotra@users.noreply.github.com>
2024-11-27 03:10:45 +0000

    Sync LeetCode submission - Search a 2D Matrix (cpp)

--- problems/search_a_2d_matrix/solution.cpp
+++ problems/search_a_2d_matrix/solution.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int low = 0;
-        int high = matrix.size()-1;
-        int col = matrix[0].size()-1;
-        
-        while(low<=high){
-            int mid = low + (high-low)/2;
-            if(matrix[mid][col] < target){
-                low = mid + 1;
-            }
-            else if(matrix[mid][col] >= target){
-                high = mid - 1;
-            }
-        }
-        if(low==matrix.size()){
-            return false;
-        }
-        int row = low;
-        low = 0;
-        high = matrix[0].size()-1;
-        while(low<=high){
-            int mid = low + (high-low)/2;
-            if(matrix[row][mid]<target){
-                low = mid + 1;
-            }
-            else if(matrix[row][mid]>target){
-                high =  mid - 1;
+        int start = 0;
+        int end = matrix.size()-1;
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(matrix[mid][0]<= target){
+                start = mid+1;
             }
             else{
-                return true;
-            } 
+                end = end-1;
+            }
         }
-        
+        cout<<end<<endl;
+        if(end<=-1){
+            return false;
+        }
+        int idx = end;
+        start =0;
+        end = matrix[idx].size()-1;
+        while(start<=end){
+            int mid = (start+end)/2;
+            cout<<mid<<endl;
+            if(matrix[idx][mid]==target){
+                return true;
+            }
+            else if(matrix[idx][mid]<target){
+                start = mid +1;
+            }
+            else{
+                end = end-1;
+            }
+        }
         return false;
     }
 };
